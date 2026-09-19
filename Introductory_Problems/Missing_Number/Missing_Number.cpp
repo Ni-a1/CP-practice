e7aa7242a55258402b9904b8c4412546afff5dd6
Ni-a1 <nityasingla1@gmail.com>
2026-09-19 13:38:26 +0530

    Add C++ solution for Missing_Number

--- Introductory_Problems/Missing_Number/Missing_Number.cpp
+++ Introductory_Problems/Missing_Number/Missing_Number.cpp
@@ -5,18 +5,40 @@
  * Date: 2026-09-19
  */
 
+/*You are given all numbers between 1,2,\ldots,n except one. Your task is to find the missing number.
+Input
+The first input line contains an integer n.
+The second line contains n-1 numbers. Each number is distinct and between 1 and n (inclusive).
+Output
+Print the missing number.
+Constraints
+ 
+2 \le n \le 2 \cdot 10^5
+ 
+Example
+Input:
+5
+2 3 1 5
+ 
+Output:
+4*/
+ 
 #include<bits/stdc++.h>
 using namespace std;
-int main(){
-int n;
+int main ()
+{set<int> s;
+int n ;
 cin>>n;
-set<int>s;
 for(int i=1;i<n;i++)
 {int x;
-cin>>x;
+    cin>>x;
 s.insert(x);}
-for(int i=1;i<=n;i++){
-if(s.find(i)==s.end())
-{cout<<i;
-break;}
-}}
+int i=1;
+for(auto value:s)
+{
+if(value!=i) {cout<<i; break;}
+i++;}
+cout<<n;
+ 
+ 
+}
