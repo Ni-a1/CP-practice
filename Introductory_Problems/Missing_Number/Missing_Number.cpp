bd088b615f79fbf401134706ddc0f3bb2f8fd67e
Ni-a1 <nityasingla1@gmail.com>
2026-09-19 13:28:21 +0530

    Add C++ solution for Missing_Number

---
+++ Introductory_Problems/Missing_Number/Missing_Number.cpp
@@ -0,0 +1,22 @@
+/*
+ * Problem Name: Missing_Number
+ * Language: C++
+ * Category: Introductory_Problems
+ * Date: 2026-09-19
+ */
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+int n;
+cin>>n;
+set<int>s;
+for(int i=1;i<n;i++)
+{int x;
+cin>>x;
+s.insert(x);}
+for(int i=1;i<=n;i++){
+if(find(s.begin(),s.end(),i)==s.end())
+{cout<<i;
+break;}
+}}
